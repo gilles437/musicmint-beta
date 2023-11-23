35cfc5f1c8682aa60796defc10ddf14b9bec959c
Topdevbeast <topdevbeast@gmail.com>
2023-11-23 18:36:40 +0900

    back all

--- minted-frontend/src/hooks/useProvider.ts
+++ minted-frontend/src/hooks/useProvider.ts
@@ -3,8 +3,7 @@ import { WsProvider } from "@polkadot/api";
 
 export const useProvider = () => {
   return useMemo(() => {
-    // const wsProvider = new WsProvider("wss://rpc-test.allfeat.io");
-    const wsProvider = new WsProvider("ws://127.0.0.1:9944");
+    const wsProvider = new WsProvider("wss://rpc-test.allfeat.io");
 
     return wsProvider
   }, []);
