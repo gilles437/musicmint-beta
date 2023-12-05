6c425c97c682ab88eeb6ddc8ea31408d19a672f6
TopCodeBeast <maxb47163@gmail.com>
2023-12-05 21:59:57 +0900

    edit album

--- minted-frontend/src/components/album/form/AlbumForm.tsx
+++ minted-frontend/src/components/album/form/AlbumForm.tsx
@@ -1,39 +1,60 @@
-import React, { useState } from "react";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Link from "next/link";
+import React, { useEffect, useMemo, useState } from 'react'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { useSelector, selectAlbums } from '@/lib/redux'
+import { useAlbumMetadata } from '@/hooks/useAlbumMetadata'
 
 export type CreateAlbumInput = {
-  title: string;
-  description: string;
-  image: File;
-  maxSupply: number;
-  price: string;
-};
+  title: string
+  description: string
+  image: File
+  maxSupply: number
+  price: string
+}
 
 type Props = {
-  onSubmit: (input: CreateAlbumInput) => Promise<boolean>;
-};
+  albumId?: string
+  onSubmit: (input: CreateAlbumInput) => Promise<boolean>
+}
 
-const CreateAlbumForm = ({ onSubmit }: Props) => {
-  const [title, setTitle] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
-  const [maxSupply, setMaxSupply] = useState<number>(0);
-  const [price, setPrice] = useState<string>("");
-  const [selectedImage, setSelectedImage] = useState<File>();
-  const [showSongs, setShowSongs] = useState(false);
-  const [selectedImageFileCid, setSelectedImageFileCid] = useState<string>("");
-  const [isLoading, setIsLoading] = useState(false);
+const CreateAlbumForm = ({ albumId, onSubmit }: Props) => {
+  const albums = useSelector(selectAlbums)
+
+  const [title, setTitle] = useState<string>('')
+  const [description, setDescription] = useState<string>('')
+  const [maxSupply, setMaxSupply] = useState<number>(0)
+  const [price, setPrice] = useState<string>('')
+  const [selectedImage, setSelectedImage] = useState<File>()
+  const [selectedImageFileCid, setSelectedImageFileCid] = useState<string>('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const selectedAlbum = useMemo(() => {
+    if (albumId) {
+      return (albums || []).find((i) => i.id === albumId)
+    }
+    return null
+  }, [albums, albumId])
+
+  const metadata = useAlbumMetadata(selectedAlbum)
+
+  useEffect(() => {
+    if (selectedAlbum && metadata) {
+      setTitle(metadata.title)
+      setDescription(metadata.description)
+      setPrice(metadata.price)
+      setMaxSupply(selectedAlbum.maxsupply)
+    }
+  }, [selectedAlbum, metadata])
 
   const handleImageChange = (e: any) => {
-    e.preventDefault();
-    setSelectedImage(e.target.files[0]);
-  };
+    e.preventDefault()
+    setSelectedImage(e.target.files[0])
+  }
 
   const handleSubmit = (e: any) => {
-    e.preventDefault();
+    e.preventDefault()
     if (!validateFields()) {
-      return;
+      return
     }
 
     const input = {
@@ -42,47 +63,47 @@ const CreateAlbumForm = ({ onSubmit }: Props) => {
       image: selectedImage,
       maxSupply,
       price,
-    } as CreateAlbumInput;
+    } as CreateAlbumInput
 
     onSubmit(input).then((success) => {
-      success && emptyFields();
-    });
-  };
+      success && emptyFields()
+    })
+  }
 
   const emptyFields = () => {
-    setDescription("");
-    setTitle("");
-    setSelectedImage(undefined);
-  };
+    setDescription('')
+    setTitle('')
+    setSelectedImage(undefined)
+  }
 
   const validateFields = () => {
     if (!title) {
-      toastFunction("Please provide a title");
-      return false;
+      toastFunction('Please provide a title')
+      return false
     }
     if (!description) {
-      toastFunction("Please provide a description");
-      return false;
+      toastFunction('Please provide a description')
+      return false
     }
     if (!price) {
-      toastFunction("Please provide a price");
-      return false;
+      toastFunction('Please provide a price')
+      return false
     }
     if (!selectedImage) {
-      toastFunction("Please provide an image");
-      return false;
+      toastFunction('Please provide an image')
+      return false
     }
-    return true;
-  };
+    return true
+  }
 
   const toastFunction = (string: any) => {
-    toast.info(string, { position: toast.POSITION.TOP_RIGHT });
-  };
+    toast.info(string, { position: toast.POSITION.TOP_RIGHT })
+  }
 
   return (
     <div>
       <div className="text-center mb-3">
-        <h2>Create Album</h2>
+        <h2>{albumId ? 'Edit Album' : 'Create Album'}</h2>
       </div>
       <div className="row">
         <div className="col-md-6 col-sm-12">
@@ -91,8 +112,8 @@ const CreateAlbumForm = ({ onSubmit }: Props) => {
             <input
               type="text"
               placeholder="Enter Title..."
-              style={{ width: "70%", height: "100%" }}
-              value={title ? title : ""}
+              style={{ width: '70%', height: '100%' }}
+              value={title ? title : ''}
               onChange={(e: any) => setTitle(e.target.value)}
             />
           </div>
@@ -101,9 +122,9 @@ const CreateAlbumForm = ({ onSubmit }: Props) => {
             <textarea
               cols={30}
               rows={3}
-              value={description ? description : ""}
+              value={description ? description : ''}
               onChange={(e: any) => setDescription(e.target.value)}
-              style={{ width: "70%", height: "100%" }}
+              style={{ width: '70%', height: '100%' }}
             />
           </div>
           <div className="mt-3">
@@ -120,7 +141,7 @@ const CreateAlbumForm = ({ onSubmit }: Props) => {
               type="number"
               min={1}
               placeholder="Enter Max Supply..."
-              value={maxSupply ? maxSupply : ""}
+              value={maxSupply ? maxSupply : ''}
               onChange={(e: any) => setMaxSupply(e.target.value)}
             />
           </div>
@@ -129,16 +150,16 @@ const CreateAlbumForm = ({ onSubmit }: Props) => {
             <input
               type="text"
               placeholder="Enter Price..."
-              value={price ? price : ""}
+              value={price ? price : ''}
               onChange={(e: any) => setPrice(e.target.value)}
             />
             <span className="ms-3">AFT</span>
           </div>
         </div>
         <div className="col-md-6 col-sm-12">
-          {showSongs && (
+          {!!metadata && (
             <div>
-              <img src={`https://ipfs.io/ipfs/${selectedImageFileCid}`}></img>
+              <img src={metadata.image || '/images/album.png'}></img>
             </div>
           )}
         </div>
@@ -148,13 +169,13 @@ const CreateAlbumForm = ({ onSubmit }: Props) => {
         <button
           className="btn rounded-3 color-000 fw-bold border-1 border brd-light bg-yellowGreen"
           onClick={handleSubmit}
-          disabled={showSongs || isLoading}
+          disabled={isLoading}
         >
-          Create Album
+          {albumId ? 'Update' : 'Create'} Album
         </button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CreateAlbumForm;
+export default CreateAlbumForm
