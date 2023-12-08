60e7a7b05d9fca7964c5af26d7070c10b581ccaa
TopCodeBeast <maxb47163@gmail.com>
2023-12-08 21:07:50 +0900

    form validation

--- minted-frontend/src/sections/Album/AlbumForm.tsx
+++ minted-frontend/src/sections/Album/AlbumForm.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
-import { toast } from 'react-toastify';
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Row';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import { Album, AlbumMetadata } from '@/lib/redux';
 import LoadingButton from '@/components/LoadingButton';
 
@@ -20,107 +23,65 @@ type Props = {
 };
 
 const AlbumForm = ({ album, metadata, onSubmit }: Props) => {
-  const [title, setTitle] = useState<string>('');
-  const [description, setDescription] = useState<string>('');
-  const [maxSupply, setMaxSupply] = useState<number>(0);
-  const [price, setPrice] = useState<string>('');
-  const [selectedImage, setSelectedImage] = useState<File>();
   const [isLoading, setIsLoading] = useState(false);
-  const [validated, setValidated] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      description: '',
+      image: null,
+      maxSupply: 0,
+      price: '',
+    },
+    validationSchema: formSchema,
+    onSubmit: (values) => {
+      handleSubmit(values);
+    },
+  });
 
   useEffect(() => {
     if (album && metadata) {
-      setTitle(metadata.title);
-      setDescription(metadata.description);
-      setPrice(metadata.price);
-      setMaxSupply(album.maxsupply);
+      formik.setFieldValue('title', metadata.title);
+      formik.setFieldValue('description', metadata.description);
+      formik.setFieldValue('maxSupply', album.maxsupply);
+      formik.setFieldValue('price', metadata.price);
     }
-  }, [album, metadata]);
+  }, [formik, album, metadata]);
 
-  const handleImageChange = (e: any) => {
-    e.preventDefault();
-    setSelectedImage(e.target.files[0]);
-  };
-
-  const handleSubmit = (e: any) => {
-    const form = e.currentTarget;
-    console.log('handleSubmit', form);
-    if (form.checkValidity() === false) {
-      console.log('handleSubmit--error');
-      e.preventDefault();
-      e.stopPropagation();
-      return;
-    }
-
-    console.log('handleSubmit--validated');
-    setValidated(true);
-
-    if (!validateFields()) {
-      return;
-    }
-
-    const input = {
-      title,
-      description,
-      image: selectedImage,
-      maxSupply,
-      price,
-    } as CreateAlbumInput;
-
+  const handleSubmit = (values: CreateAlbumInput) => {
     setIsLoading(true);
 
-    onSubmit(input).then((success) => {
+    onSubmit(values).then((success) => {
       success && emptyFields();
       setIsLoading(false);
     });
   };
 
-  const emptyFields = () => {
-    setDescription('');
-    setTitle('');
-    setSelectedImage(undefined);
-  };
-
-  const validateFields = () => {
-    if (!title) {
-      toast.warn('Please provide a title');
-      return false;
-    }
-    if (!description) {
-      toast.warn('Please provide a description');
-      return false;
-    }
-    if (!price) {
-      toast.warn('Please provide a price');
-      return false;
-    }
-    if (!selectedImage) {
-      toast.warn('Please provide an image');
-      return false;
-    }
-    return true;
-  };
+  const emptyFields = () => {};
 
   return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+    <Form noValidate onSubmit={formik.handleSubmit}>
       <div className="text-center mb-3 pb-3">
         <h2>{album ? 'Edit Album' : 'Create Album'}</h2>
       </div>
-      <div className="row">
+      <Row className="mt-3">
         <div className="col-md-6 col-sm-12">
-          <Form.Group className="mt-3" controlId="title">
+          <Form.Group as={Col} md="12" className="mt-3 position-relative" controlId="title">
             <Form.Label>Title</Form.Label>
             <Form.Control
-              required
               type="text"
+              name="title"
               placeholder="Album Title"
-              value={title || ''}
-              onChange={(e: any) => setTitle(e.target.value)}
+              isInvalid={!!(formik.touched.title && formik.errors.title)}
+              value={formik.values.title}
+              onChange={formik.handleChange}
             />
-            <Form.Control.Feedback type="invalid">Title is required</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid" tooltip>
+              {formik.errors.title}
+            </Form.Control.Feedback>
           </Form.Group>
 
-          <Form.Group className="mt-3" controlId="description">
+          <Form.Group as={Col} md="12" className="mt-3 position-relative" controlId="description">
             <Form.Label>Description</Form.Label>
             <Form.Control
               required
@@ -128,24 +89,30 @@ const AlbumForm = ({ album, metadata, onSubmit }: Props) => {
               rows={3}
               cols={30}
               placeholder="Description"
-              value={description || ''}
-              onChange={(e: any) => setDescription(e.target.value)}
+              isInvalid={!!(formik.touched.description && formik.errors.description)}
+              value={formik.values.description}
+              onChange={formik.handleChange}
             />
-            <Form.Control.Feedback>Description is required</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid" tooltip>
+              {formik.errors.description}
+            </Form.Control.Feedback>
           </Form.Group>
 
-          <Form.Group className="mt-3" controlId="image">
+          <Form.Group as={Col} md="12" className="mt-3 position-relative" controlId="image">
             <Form.Label>Image</Form.Label>
             <Form.Control
               required
               type="file"
               accept=".jpg, .png, .jpeg, .gif, .bmp, .tif, .tiff|image/*"
-              onChange={handleImageChange}
+              isInvalid={!!(formik.touched.image && formik.errors.image)}
+              onChange={(e: any) => formik.setFieldValue('image', e.target.files[0])}
             />
-            <Form.Control.Feedback>Image is required</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid" tooltip>
+              {formik.errors.image}
+            </Form.Control.Feedback>
           </Form.Group>
 
-          <Form.Group className="mt-3" controlId="maxSupply">
+          <Form.Group as={Col} md="12" className="mt-3 position-relative" controlId="maxSupply">
             <Form.Label>Max Supply</Form.Label>
             <Form.Control
               required
@@ -153,24 +120,32 @@ const AlbumForm = ({ album, metadata, onSubmit }: Props) => {
               type="number"
               min={1}
               placeholder="Max Supply"
-              value={maxSupply || ''}
-              onChange={(e: any) => setMaxSupply(Number(e.target.value))}
+              isInvalid={!!(formik.touched.maxSupply && formik.errors.maxSupply)}
+              value={formik.values.maxSupply}
+              onChange={formik.handleChange}
             />
-            <Form.Control.Feedback>Max Supply is required</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid" tooltip>
+              {formik.errors.maxSupply}
+            </Form.Control.Feedback>
           </Form.Group>
 
-          <Form.Group className="mt-3" controlId="price">
+          <Form.Group as={Col} md="12" className="mt-3 position-relative" controlId="price">
             <Form.Label>Price (AFT)</Form.Label>
             <Form.Control
               required
               as="input"
               type="number"
               min={1}
-              value={price || ''}
               placeholder="Album Price"
-              onChange={(e: any) => setPrice(e.target.value)}
+              isInvalid={!!(formik.touched.price && formik.errors.price)}
+              value={formik.values.price}
+              onChange={formik.handleChange}
             />
-            <Form.Control.Feedback>Price is required</Form.Control.Feedback>
+            {!!formik.touched.price && (
+              <Form.Control.Feedback type="invalid" tooltip>
+                {formik.errors.price}
+              </Form.Control.Feedback>
+            )}
           </Form.Group>
         </div>
         <div className="col-md-6 col-sm-12">
@@ -185,7 +160,7 @@ const AlbumForm = ({ album, metadata, onSubmit }: Props) => {
             </div>
           )}
         </div>
-      </div>
+      </Row>
 
       <div className="row">
         <div className="col-md-6 col-sm-12">
@@ -199,5 +174,13 @@ const AlbumForm = ({ album, metadata, onSubmit }: Props) => {
     </Form>
   );
 };
+
+const formSchema = yup.object().shape({
+  title: yup.string().required('Title required'),
+  description: yup.string().required('Description required'),
+  image: yup.mixed().required('Image required'),
+  maxSupply: yup.number().required('Max Supply required'),
+  price: yup.string().required('Price required'),
+});
 
 export default AlbumForm;
