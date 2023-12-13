26d538b76ef61e4b7dfa6f647a536a94acdf9451
TopCodeBeast <maxb47163@gmail.com>
2023-12-13 14:38:38 +0900

    show artists

--- minted-frontend/src/components/Home/Features.tsx
+++ minted-frontend/src/components/Home/Features.tsx
@@ -1,30 +1,26 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Autoplay } from 'swiper';
-import { getActiveAccount } from '@/utils/account';
-
-// import CountTo from '../CountTo';
-import features from '@/data/NFTMarketplace/features.json';
 
 import {
   useSelector,
   useDispatch,
-  selectAlbums,
   fetchOwnedAlbumListAsync,
   fetchArtistListAsync,
+  selectArtists,
 } from '@/lib/redux';
 
-import "swiper/css";
-import "swiper/css/autoplay";
-import AlbumItem from '../Album/AlbumItem';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import ArtistAvatar from '../Admin/ArtistAvatar';
 
 SwiperCore.use([Autoplay]);
 
 const Features = () => {
   const [load, setLoad] = useState(false);
-  const [position] = useState({ from: 2150, to: 2500 });
+  // const [position] = useState({ from: 2150, to: 2500 });
   const dispatch = useDispatch();
-  const albums = useSelector(selectAlbums);
+  const artists = useSelector(selectArtists);
 
   const fetchAlbumList = useCallback(
     (owner: string) => {
@@ -35,8 +31,8 @@ const Features = () => {
 
   useEffect(() => {
     dispatch(fetchArtistListAsync());
-    const account = getActiveAccount();
-    fetchAlbumList(account);
+    // const account = getActiveAccount();
+    // fetchAlbumList(account);
   }, [dispatch, fetchAlbumList]);
 
   useEffect(() => {
@@ -52,65 +48,78 @@ const Features = () => {
             <span className="mx-2"> featured </span>
             <img src="/assets/img/icons/star2.png" alt="" className="icon" />
           </h6>
-          <h2> Best <span> Sellers </span> </h2>
+          <h2>
+            {' '}
+            Best <span> Sellers </span>{' '}
+          </h2>
         </div>
         <div className="content">
           <div className="row justify-content-center">
-            {
-              albums.map((album, index) => <AlbumItem album={album} key={index}/>)
-            }
+            {(artists || []).map((artist, index) => (
+              <ArtistAvatar artist={artist} key={index} />
+            ))}
           </div>
         </div>
       </div>
       <div className="features-line-slider12">
-        {
-          load && (
-            <Swiper
-              className="swiper-container"
-              spaceBetween={0}
-              centeredSlides={true}
-              speed={10000}
-              autoplay={{
-                delay: 1
-              }}
-              loop={true}
-              breakpoints={{
-                0: {
-                  slidesPerView: 1,
-                },
-                480: {
-                  slidesPerView: 1,
-                },
-                787: {
-                  slidesPerView: 1,
-                },
-                991: {
-                  slidesPerView: 2,
-                },
-                1200: {
-                  slidesPerView: 2,
-                }
-              }}
-            >
-              <SwiperSlide className="swiper-slide">
-                <a href="#"> <img src="/assets/img/icons/star3.png" alt="" /> <h2> Sell your nft </h2> </a>
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide">
-                <a href="#"> <img src="/assets/img/icons/star3.png" alt="" /> <h2> be an portfolio author </h2> </a>
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide">
-                <a href="#"> <img src="/assets/img/icons/star3.png" alt="" /> <h2> nft’s great </h2> </a>
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide">
-                <a href="#"> <img src="/assets/img/icons/star3.png" alt="" /> <h2> Sell your nft </h2> </a>
-              </SwiperSlide>
-            </Swiper>
-          )
-        }
+        {load && (
+          <Swiper
+            className="swiper-container"
+            spaceBetween={0}
+            centeredSlides={true}
+            speed={10000}
+            autoplay={{
+              delay: 1,
+            }}
+            loop={true}
+            breakpoints={{
+              0: {
+                slidesPerView: 1,
+              },
+              480: {
+                slidesPerView: 1,
+              },
+              787: {
+                slidesPerView: 1,
+              },
+              991: {
+                slidesPerView: 2,
+              },
+              1200: {
+                slidesPerView: 2,
+              },
+            }}
+          >
+            <SwiperSlide className="swiper-slide">
+              <a href="#">
+                {' '}
+                <img src="/assets/img/icons/star3.png" alt="" /> <h2> Sell your nft </h2>{' '}
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="swiper-slide">
+              <a href="#">
+                {' '}
+                <img src="/assets/img/icons/star3.png" alt="" /> <h2> be an portfolio author </h2>{' '}
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="swiper-slide">
+              <a href="#">
+                {' '}
+                <img src="/assets/img/icons/star3.png" alt="" /> <h2> nft’s great </h2>{' '}
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="swiper-slide">
+              <a href="#">
+                {' '}
+                <img src="/assets/img/icons/star3.png" alt="" /> <h2> Sell your nft </h2>{' '}
+              </a>
+            </SwiperSlide>
+          </Swiper>
+        )}
         <img src="/assets/img/icons/features/27.png" alt="" className="icon slide_up_down" />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
