dd5185eb08e646da912ded7dfb83ca9c2f6e118f
TopCodeBeast <maxb47163@gmail.com>
2023-12-15 14:45:21 +0900

    update collection card

--- minted-frontend/src/components/Home/CollectionCard.tsx
+++ minted-frontend/src/components/Home/CollectionCard.tsx
@@ -1,41 +1,45 @@
 import React from 'react';
+import Link from 'next/link';
 import { Album } from '@/lib/redux';
 import { useAlbumMetadata } from '@/hooks/useAlbumMetadata';
-import collectionsData from '@/data/NFTMarketplace/collections.json';
+import { useArtistMetadata } from '@/hooks/useArtistMetadata';
 
 type Props = {
   album: Album;
 };
 
-const collection = collectionsData[0];
-const subImages = collection.subImages;
-
 const CollectionCard = ({ album }: Props) => {
-  const metadata = useAlbumMetadata(album);
+  const albumInfo = useAlbumMetadata(album);
+  const artistInfo = useArtistMetadata(album.from);
 
   return (
     <div className="collection-card">
-      <div className="top-info">
-        <h6> {metadata?.title || ''} </h6>
-        <p>
-          <img src="/assets/img/icons/star2.png" alt="" />
-          <span className="text-white"> {12} </span>
-          <span style={{ paddingLeft: 4 }}> Items </span>
-        </p>
-      </div>
-      <div className="auther-img">
-        <img src={collection.pic} alt="" />
-      </div>
-      <div className="imgs">
-        <div className="main-img img-cover">
-          <img src={metadata?.image || ''} alt="" />
+      <Link
+        href={`/album/detail?contract=${album.contract}&albumId=${album.albumid}`}
+        className="project-card hover-shadow"
+      >
+        <div className="top-info">
+          <h6> {albumInfo?.title || ''} </h6>
+          <p>
+            <img src="/assets/img/icons/star2.png" alt="" />
+            <span className="text-white"> {12} </span>
+            <span style={{ paddingLeft: 4 }}> Items </span>
+          </p>
         </div>
-        {/* <div className="sub-imgs">
+        <div className="auther-img">
+          <img src={artistInfo?.image || '/assets/img/user-1.jpg'} alt="" />
+        </div>
+        <div className="imgs">
+          <div className="main-img img-cover">
+            <img src={albumInfo?.image || ''} alt="" />
+          </div>
+          {/* <div className="sub-imgs">
           {subImages.map((img, idx) => (
             <img src={img} alt="" key={idx} />
           ))}
         </div> */}
-      </div>
+        </div>
+      </Link>
     </div>
   );
 };
