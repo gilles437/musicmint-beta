bfecae644cde3cd087a5686855220229f40647ce
TopCodeBeast <maxb47163@gmail.com>
2023-12-20 18:42:09 +0900

    update album profile

--- minted-frontend/src/sections/Album/EditAlbum.tsx
+++ minted-frontend/src/sections/Album/EditAlbum.tsx
@@ -1,7 +1,12 @@
 import React, { useCallback } from 'react';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
 
-import { Album } from '@/lib/redux';
+import { Album, AlbumMetadata, fetchAlbumByIdAsync, useDispatch } from '@/lib/redux';
 import { useAlbum } from '@/hooks/useAlbum';
+import { useAlbumMetadata } from '@/hooks/useAlbumMetadata';
+import { createIpfsUrl } from '@/utils/ipfs';
+import { uploadFile, uploadMetadata } from '@/utils/bucket';
 import EditAlbumForm, { CreateAlbumInput } from './AlbumForm';
 
 type Props = {
@@ -9,60 +14,70 @@ type Props = {
 };
 
 const EditAlbum = ({ album }: Props) => {
-  const { createAlbum } = useAlbum(album?.contract);
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const { setTokenUri } = useAlbum(album?.contract);
+  const metadata = useAlbumMetadata(album);
+
+  const onAlbumUpdated = async (albumId: number) => {
+    console.log('onAlbumUpdated', albumId);
+    dispatch(fetchAlbumByIdAsync({ contract: album.contract, albumId }));
+
+    const timerId = setTimeout(() => {
+      router.push(`/album/owned`);
+    }, 2000);
+    return () => clearTimeout(timerId);
+  };
 
   const handleUpdateAlbum = useCallback(
     async (input: CreateAlbumInput) => {
       console.log('handleUpdateAlbum', input);
-      /*try {
-      let imageCid = null;
-      if (input.image) {
-        imageCid = await uploadFile(input.image);
-        if (!imageCid) {
-          console.error('error when uploading image nft');
+      try {
+        let imageCid = null;
+        if (input.file && input.file instanceof File) {
+          imageCid = await uploadFile(input.file);
+          if (!imageCid) {
+            console.error('error when uploading image nft');
+            return false;
+          }
+        }
+        console.log('imageCid', imageCid)
+
+        const meta: AlbumMetadata = {
+          name: input.file?.name?.toString() || metadata?.name,
+          title: input.title,
+          description: input.description,
+          price: input.price,
+          image: imageCid ? createIpfsUrl(imageCid) : metadata?.image || '',
+        };
+        const metadataId = await uploadMetadata(meta);
+        if (!metadataId) {
+          console.error('error when uploading metadata');
+          return false;
+        }
+
+        const metaUrl = createIpfsUrl(metadataId);
+        toast.info(`Album Metadata saved on ${metaUrl}`);
+
+        const albumId = await setTokenUri(album.albumid, metaUrl);
+        console.log('success', albumId);
+
+        if (albumId !== null && albumId !== undefined && albumId >= 0) {
+          onAlbumUpdated(albumId);
+          toast.info(`New Album TokenId is: ${Number(albumId)}`);
+          return true;
+        }
+      } catch (err: any) {
+        if (err && err.message === 'Cancelled') {
+          toast.error(`Transaction cancelled`);
           return false;
         }
       }
-
-      const imageUrl = albumMetadata?.image || '';
-      const meta: AlbumMetadata = {
-        name: input.image ? input.image.name.toString() : '',
-        title: input.title,
-        description: input.description,
-        price: input.price,
-        image: imageCid ? createIpfsUrl(imageCid) : imageUrl,
-      };
-      const metadataId = await uploadMetadata(meta);
-      if (!metadataId) {
-        console.error('error when uploading metadata');
-        return false;
-      }
-
-      const metaUrl = createIpfsUrl(metadataId);
-      const success = await createAlbum(
-        Number(input.maxSupply),
-        Number(input.price),
-        metaUrl,
-        (albumId: number) => {
-          toast.info(`New Album TokenId is: ${Number(albumId)}`);
-        }
-      );
-      console.log('success', success);
-
-      if (success) {
-        toast.info(`Album Metadata saved on ${metaUrl}`);
-        return true;
-      } else {
-        toast.error(`Something wrong to create Album`);
-      }
-    } catch (error) {
-      console.log(error);
-    }*/
+      
+      toast.error(`Something wrong to update album`);
       return false;
     },
-    [
-      /*createAlbum*/
-    ]
+    [setTokenUri]
   );
 
   return <EditAlbumForm album={album} onSubmit={handleUpdateAlbum} />;
