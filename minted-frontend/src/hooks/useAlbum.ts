29b0e97bc7cf43556de3e96dfb979601ada9fbca
TopCodeBeast <maxb47163@gmail.com>
2023-12-20 17:36:39 +0900

    add setTokenUri method

--- minted-frontend/src/hooks/useAlbum.ts
+++ minted-frontend/src/hooks/useAlbum.ts
@@ -113,6 +113,49 @@ export const useAlbum = (contractAddress?: string) => {
     [request, contract, chainDecimals]
   );
 
+  const setTokenUri = useCallback(
+    async (albumId: number, metaUrl: string): Promise<number | null> => {
+      return new Promise<number | null>(async (resolve, reject) => {
+        try {
+          if (!request || !contract) {
+            return reject('API is not ready');
+          }
+
+          const { signer, options, account } = request;
+
+          const queryTx = await contract.query.setTokenUri(
+            account,
+            options,
+            albumId,
+            metaUrl
+          );
+
+          if (!queryTx.result?.isOk) {
+            console.error('****queryTx.error', queryTx.result.asErr);
+            return reject(queryTx.result.asErr);
+          }
+
+          const tx = await contract.tx.setTokenUri(options, albumId, metaUrl);
+          console.log('*****tx=', tx);
+
+          const unsub = await tx.signAndSend(account, signer, (result) => {
+            console.log('*****tx**result=', result.status.isFinalized);
+            if (!result.status.isFinalized) {
+              return;
+            }
+
+            unsub();
+            resolve(albumId);
+          });
+        } catch (err) {
+          console.error(err);
+          reject(err);
+        }
+      });
+    },
+    [request, contract, chainDecimals]
+  );
+
   const deleteAlbum = useCallback(
     async (albumId: number, contractAddress: string): Promise<number | null> => {
       return new Promise<number>(async (resolve, reject) => {
@@ -203,6 +246,7 @@ export const useAlbum = (contractAddress?: string) => {
 
   return {
     createAlbum,
+    setTokenUri,
     deleteAlbum,
     mintAlbum,
   };
